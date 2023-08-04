8cd451607ae0ccb54ea8576e69631383812e17d1
Ettore Moretti <ettoremoretti27@gmail.com>
2023-08-04 16:27:03 +0200

    final commit

---
+++ lemon-web-app/src/components/pages/NotFound/index.js
@@ -0,0 +1,11 @@
+import './index.css';
+
+const NotFound = () => {
+  return (
+    <div className="container page-not-found">
+      <h2>Sorry! Page not found</h2>
+    </div>
+  );
+};
+
+export default NotFound;
